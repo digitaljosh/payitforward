ab8daa189c2d7ab9885fe906baeda10ab54c2928
digitaljosh <joshuajmarkus@gmail.com>
2017-10-02 20:37:26 -0700

    Initial Commit

---
+++ src/main/java/com/example/payitforward/PayitforwardApplication.java
@@ -0,0 +1,12 @@
+package com.example.payitforward;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayitforwardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayitforwardApplication.class, args);
+	}
+}

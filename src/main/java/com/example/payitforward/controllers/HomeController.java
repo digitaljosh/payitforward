6dc914337bbb56df1766757cdbcf07075935b7f9
digitaljosh <joshuajmarkus@gmail.com>
2017-10-16 20:23:01 -0700

    HomeController & Responsive View

--- src/main/java/com/example/payitforward/controllers/HomeController.java
+++ src/main/java/com/example/payitforward/controllers/HomeController.java
@@ -1,8 +1,5 @@
 package com.example.payitforward.controllers;
 
-import com.example.payitforward.models.data.OpportunityDao;
-import com.example.payitforward.models.data.UserDao;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,17 +8,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 
-    @Autowired
-    private OpportunityDao opportunityDao;
-
-    @Autowired
-    private UserDao userDao;
 
     @RequestMapping(value = "")
     public String index(Model model) {
-
-        model.addAttribute("opportunities", opportunityDao.findAll());
-        model.addAttribute("title", "Current Opportunities");
 
         return "index";
     }

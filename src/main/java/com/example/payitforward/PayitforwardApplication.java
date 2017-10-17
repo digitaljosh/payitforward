0930345cb04ad78afba43f4476231b144afc9d13
digitaljosh <joshuajmarkus@gmail.com>
2017-10-16 20:06:57 -0700

    Git conformity

--- src/main/java/com/example/payitforward/PayitforwardApplication.java
+++ src/main/java/com/example/payitforward/PayitforwardApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PayitforwardApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(PayitforwardApplication.class, args);
 	}
 }

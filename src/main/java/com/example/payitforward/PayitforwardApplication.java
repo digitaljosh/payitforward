e7c41cc32df0aab71d10f1b0d92b75aa06fa89fd
Josh Markus <joshuajmarkus@gmail.com>
2017-10-16 21:16:43 -0700

    Update PayitforwardApplication.java

--- src/main/java/com/example/payitforward/PayitforwardApplication.java
+++ src/main/java/com/example/payitforward/PayitforwardApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PayitforwardApplication {
 
-	public static void main(String[] args) {SpringApplication.run(PayitforwardApplication.class, args);
+	public static void main(String[] args) {
+		SpringApplication.run(PayitforwardApplication.class, args);
 	}
 }

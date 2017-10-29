c39cebf9a3accdef0ac2bb67f09d0e0cbe63458b
Ruby Arbogast <ruby.arbogast@gmail.com>
2017-10-29 13:24:19 -0700

    Change file name

--- src/main/java/com/example/payitforward/controllers/UploadController.java
+++ src/main/java/com/example/payitforward/controllers/UploadController.java
@@ -1,5 +1,6 @@
 package com.example.payitforward.controllers;
 
+import com.example.payitforward.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -7,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.servlet.http.HttpSession;
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -18,14 +21,27 @@ public class UploadController {
     //Save the uploaded file to this folder
     private static String UPLOADED_FOLDER = "C://temp//";
 
+    private final Path rootLocation = Paths.get("upload-dir");
+
     @GetMapping("profile/upload")
-    public String index() {
+    public String index(HttpSession session) {
+
+        if (session.getAttribute("loggedInUser") == null){
+            return "redirect:/login";
+        }
         return "profile/upload";
     }
 
     @PostMapping("profile/upload")
     public String singleFileUpload(@RequestParam("file") MultipartFile file,
-                                   RedirectAttributes redirectAttributes) {
+                                   RedirectAttributes redirectAttributes, HttpSession session) {
+
+        //TODO: get user ID
+        User currentUser = (User) session.getAttribute("loggedInUser");
+        String currentId = String.valueOf(currentUser.getId());
+
+        //TODO: create folder based on user ID
+        File dir = new File(currentId);
 
         if (file.isEmpty()) {
             redirectAttributes.addFlashAttribute("message", "Please select a file to upload");
@@ -36,7 +52,8 @@ public class UploadController {
 
             // Get the file and save it somewhere
             byte[] bytes = file.getBytes();
-            Path path = Paths.get(UPLOADED_FOLDER + file.getOriginalFilename());
+
+            Path path = Paths.get(currentId + file.getOriginalFilename());
             Files.write(path, bytes);
 
             redirectAttributes.addFlashAttribute("message",

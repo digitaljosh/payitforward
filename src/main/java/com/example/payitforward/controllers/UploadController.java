8d60fa86481f4eebb1e9917a80fa13f364c3fc72
Ruby Arbogast <ruby.arbogast@gmail.com>
2017-10-29 13:06:27 -0700

    Initial commit

---
+++ src/main/java/com/example/payitforward/controllers/UploadController.java
@@ -0,0 +1,57 @@
+package com.example.payitforward.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@Controller
+public class UploadController {
+
+    //Save the uploaded file to this folder
+    private static String UPLOADED_FOLDER = "C://temp//";
+
+    @GetMapping("profile/upload")
+    public String index() {
+        return "profile/upload";
+    }
+
+    @PostMapping("profile/upload")
+    public String singleFileUpload(@RequestParam("file") MultipartFile file,
+                                   RedirectAttributes redirectAttributes) {
+
+        if (file.isEmpty()) {
+            redirectAttributes.addFlashAttribute("message", "Please select a file to upload");
+            return "redirect:/profile/uploadStatus";
+        }
+
+        try {
+
+            // Get the file and save it somewhere
+            byte[] bytes = file.getBytes();
+            Path path = Paths.get(UPLOADED_FOLDER + file.getOriginalFilename());
+            Files.write(path, bytes);
+
+            redirectAttributes.addFlashAttribute("message",
+                    "You successfully uploaded '" + file.getOriginalFilename() + "'");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return "redirect:/profile/uploadStatus";
+    }
+
+    @GetMapping("profile/uploadStatus")
+    public String uploadStatus() {
+        return "profile/uploadStatus";
+    }
+
+}
